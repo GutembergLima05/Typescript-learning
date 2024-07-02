c7211f2040c86e620afdcf674acc8eb724ff9b2c
Gutemberg Lima <j.berglima05@gmail.com>
2024-07-02 13:18:35 -0300

    feat: implemented controllers and repositories

--- api-rest/src/controllers/SubjectController.ts
+++ api-rest/src/controllers/SubjectController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express-serve-static-core"
+import { subjectRepository } from "../repositories/subjectRepository"
 
 export class SubjectController {
     async create(req: Request, res: Response){
@@ -9,7 +10,11 @@ export class SubjectController {
         }
 
         try {
+            const newSubject = subjectRepository.create({ name })
             
+            await subjectRepository.save(newSubject)
+
+            return  res.status(201).json(newSubject)
         } catch (error) {
             console.log(error)
             return res.status(500).json({message: 'Internal server error'})
